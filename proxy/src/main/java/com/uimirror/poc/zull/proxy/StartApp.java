6984d0b2d0e969eabef76948d1b4cdbc8fc9796b
jayarampradhan <jayaramimca@gmail.com>
2016-12-29 00:58:39 +0530

    Adding docker support

---
+++ proxy/src/main/java/com/uimirror/poc/zull/proxy/StartApp.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Uimirror.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Uimirror license
+ * which accompanies this distribution, and is available at
+ * http://www.uimirror.com/legal
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors: Jay
+ * Uimirror Team
+ *******************************************************************************/
+
+package com.uimirror.poc.zull.proxy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by Jay on 27/12/16.
+ */
+@SpringBootApplication
+@EnableConfigurationProperties
+public class StartApp extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        new StartApp()
+                .configure(new SpringApplicationBuilder(StartApp.class))
+                .run(args);
+    }
+
+}
